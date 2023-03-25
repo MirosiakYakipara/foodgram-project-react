95e9cb2ea618afab563cc57c00215fcf0cca1c13
Никита Зотов <nz030432@gmail.com>
2023-03-26 00:58:50 +0500

    correct settings

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -71,12 +71,12 @@ DATABASES = {
     }
 }
 
-# DATABASES = { 
-#     'default': { 
-#         'ENGINE': 'django.db.backends.sqlite3', 
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'), 
-#     } 
-# } 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
