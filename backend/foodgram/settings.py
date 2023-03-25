46485b9dd446962e2b63f9fddb6747c80453ee01
Никита Зотов <nz030432@gmail.com>
2023-03-26 01:46:54 +0500

    create dump

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -71,14 +71,6 @@ DATABASES = {
     }
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
-
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 AUTH_PASSWORD_VALIDATORS = [
