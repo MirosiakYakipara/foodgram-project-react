3011858165e39cf15867e90ecbd8cb4628e2f4b9
Никита Зотов <nz030432@gmail.com>
2023-03-26 02:03:41 +0500

    correct dump

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -60,16 +60,24 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'foodgram.wsgi.application'
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
+#         'NAME': os.getenv('DB_NAME', default=None),
+#         'USER': os.getenv('POSTGRES_USER', default=None),
+#         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default=None),
+#         'HOST': os.getenv('DB_HOST', default=None),
+#         'PORT': os.getenv('DB_PORT', default=None)
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
-        'NAME': os.getenv('DB_NAME', default=None),
-        'USER': os.getenv('POSTGRES_USER', default=None),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default=None),
-        'HOST': os.getenv('DB_HOST', default=None),
-        'PORT': os.getenv('DB_PORT', default=None)
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
